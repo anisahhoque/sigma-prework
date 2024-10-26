a8af3bc67d51ef70fc387aa15ec87584de4c17c1
anisahhoque <hoqueshabrina@gmail.com>
2024-10-27 00:27:27 +0100

    change

--- age.py
+++ age.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def get_current_date():
     return datetime.now()
 
@@ -8,15 +9,18 @@ def calculate_age(date):
     store_date_info = date.split("-")
     store_current_date = get_current_date()
 
-    age = calc_year_diff(int(store_date_info[2]),store_current_date)
+    age = calc_year_diff(int(store_date_info[2]), store_current_date)
 
-    if (store_current_date.month < int(store_date_info[1])) or  (store_current_date.month == int(store_date_info[1]) and store_current_date.day < int(store_date_info[0])):
+    if (store_current_date.month < int(store_date_info[1])) or (store_current_date.month == int(store_date_info[1]) and store_current_date.day < int(store_date_info[0])):
         return age-1
     return age
 
-def calc_year_diff(year_now,year):
+
+def calc_year_diff(year_now, year):
     return year.year - year_now
 
+
 if __name__ == "__main__":
-    #print(calculate_age("DD-MM-YYYY"))
+    # print(calculate_age("DD-MM-YYYY"))
+    print("test edit")
     pass
